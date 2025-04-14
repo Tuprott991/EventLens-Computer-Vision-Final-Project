0db711fe27dc5d0dad7a1b7e43a2a7bdeeb1ed5d
Tuprott991 <nvtu22@clc.fitus.edu.vn>
2025-04-14 18:26:20 +0700

    Training with pretrained at epoch 5

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ import copy
 JSON_PATH = '/kaggle/input/thesis-cufed/CUFED/event_type.json'
 IMAGE_ROOT = '/kaggle/input/thesis-cufed/CUFED/images'
 NUM_LABELS = 23   
-BATCH_SIZE = 12
+BATCH_SIZE = 10
 LEARNING_RATE = 5e-5
 EPOCHS = 20
 FREEZE_EPOCHS = 5
