a3ea52dc1d6f603ded8b35f923b785a936b852bd
Tuprott991 <nvtu22@clc.fitus.edu.vn>
2025-06-13 18:51:47 +0700

    Fix 1 xiu

--- deploy.py
+++ deploy.py
@@ -79,7 +79,3 @@ async def predict(files: list[UploadFile] = File(...)):
         os.rmdir(album_folder)
 
 
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("deploy:app", host="0.0.0.0", port=8000, reload=True)
